0c00ea8c7c8db6cc5fdea8c2752931361befcc6b
Francisco <fsanchezbz@gmail.com>
2023-06-25 23:27:33 +0200

    step:6 Modules - MessageToast

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
        onShowHello : function () {
           // show a native JavaScript alert
-          alert("Hello, i will learn UI5");
+          //alert("Hello, i will learn UI5");
+          MessageToast.show("Hello World");
        }
     });
  });
