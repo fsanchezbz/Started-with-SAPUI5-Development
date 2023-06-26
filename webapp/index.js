b29a172dec639dff2ef617119b9d2b62e249628e
Francisco <fsanchezbz@gmail.com>
2023-06-26 11:27:33 +0200

    Step 9: Component Configuration -After  Model-View-Controller (MVC) concept - we will encapsulate all UI assets in a component

--- webapp/index.js
+++ webapp/index.js
@@ -1,17 +1,15 @@
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-    //"sap/m/Text" // m from sap library and Text, it is the new controller
-], 
-
-function (XMLView) {
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
 	"use strict";
 
-	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App"
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
-
+	new ComponentContainer({
+		name: "sap.ui.demo.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
 
 
